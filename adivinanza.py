3ba9a2dff5ac6b21cfb0bc079479412c788ac4c5
Jesus Alberto Gonzalez Sanchez <135742583+Jesus080@users.noreply.github.com>
2024-12-23 13:51:08 -0600

    23

--- adivinanza.py
+++ adivinanza.py
@@ -13,7 +13,7 @@ def juego_adivinanza():
         intentos += 1
 
         if intento < numero_secreto:
-            print("Deemasiao Bajo. Inteenta de nuevooo.")
+            print("Deemasiao Bajo. Inteenta de nuevoo.")
         elif intento > numero_secreto:
             print("Demasiado Altoo. Intenta de nuevoo.")
         else:
