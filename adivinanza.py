82669160f8fa180a5743762e97ec25b63b40fd6f
Jesus Alberto Gonzalez Sanchez <135742583+Jesus080@users.noreply.github.com>
2025-01-07 08:05:13 -0600

    7

--- adivinanza.py
+++ adivinanza.py
@@ -18,6 +18,6 @@ def juego_adivinanza():
             print("Demasiaado Alto. Intenta de nuevoo.")
         else:
             adivinado = True
-            print(f"¡Felicidaades! Adivinaste el número {numero_secreto} en {intentos} intentos.")
+            print(f"¡Felicidades! Adivinaste el número {numero_secreto} en {intentos} intentos.")
 
 juego_adivinanza()
